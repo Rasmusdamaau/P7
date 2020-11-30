b29c8ad5e364317fd7b5bc28fce27e91f376a1cd
Rasmus <rasmus72dam@gmail.com>
2020-11-30 13:35:35 +0100

    plot funktionen gjort bedre

--- plot_GSADF.R
+++ plot_GSADF.R
@@ -1,4 +1,9 @@
-plot_GSADF <- function(u, d = NULL, d_t = NULL, p_restrict = 0.95, start_date_tq_get = "2020-01-01") {
+plot_GSADF <- function(u, d = NULL, d_t = NULL, p_restrict = 0.95, start_date_tq_get = "2020-01-01",
+                       image_name = NULL, valuta = "USD") {
+  
+  model_nr <- case_when(is.null(d_t) & is.null(d) ~ 1,
+                        is.null(d_t) ~ 2,
+                        !is.null(d_t) ~ 3)
   plot_data_combined <- combined_count(u = u, d_t = d_t, d = d, p_restrict = p_restrict)
   
   plot_data_rect <- u$result %>%
@@ -16,14 +21,30 @@ plot_GSADF <- function(u, d = NULL, d_t = NULL, p_restrict = 0.95, start_date_tq
       data = plot_data_rect, aes(xmin = date_start, xmax = date_end),
       ymin = -Inf, ymax = Inf, alpha = 0.1, inherit.aes = F
     ) +
-    scale_x_date(date_labels = "%Y %b", date_breaks = "2 month")
+    scale_x_date(date_labels = "%Y %b", date_breaks = "2 month") +
+    xlab("") +
+    ylab(paste("Price in", valuta))
+  
   
   
   
   bubble <- ggplot(data = plot_data_combined, aes(x = x, y = value, color = name)) +
     geom_line(size = 1) +
     scale_x_date(date_labels = "%Y %b", date_breaks = "2 month") +
-    theme(legend.position = "bottom")
+    theme(legend.position = "bottom") +
+    xlab("") +
+    ylab("Count")
   
-  grid.arrange(stock, bubble, nrow = 2)
+  if (model_nr == 1) {
+    bubble <- bubble + theme(legend.position = "none")
+  }
+  
+  if (!is.null(image_name)) {
+    file_name <- paste("Billeder/", image_name, ".pdf", sep = "")
+    pdf(file = file_name, height = 4,width = 8)
+    grid.arrange(stock, bubble, nrow = 2)
+    dev.off()
+    cat("image found in ", file_name)
+  } else {
+  grid.arrange(stock, bubble, nrow = 2) }
 }
