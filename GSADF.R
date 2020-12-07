25bd90f672b47be7c1a8a7997865876945fed50c
Rasmusdamaau <rasmus72dam@gmail.com>
2020-12-07 08:19:08 +0100

    Tabel funktion taget med

--- GSADF.R
+++ GSADF.R
@@ -1,78 +1,72 @@
-GSADF <- function(ticker, x = NULL, min_window = 30,
-                  step_length = 5, window_increase = 10,
-                  date_from = "1900-01-01", date_to = Sys.Date(),
-                  drift = F, trend = F, risk_free_rate = 0.01) {
-  if (is.null(x)) {
-    stock_data <- tq_get(ticker, from = date_from, to = date_to) %>%
-      select(date, adjusted) %>%
-      na.omit()
+GSADF <- function (ticker, x = NULL, min_window = 30, step_length = 5, 
+                   window_increase = 10, date_from = "1900-01-01", date_to = base::Sys.Date(), 
+                   drift = F, trend = F, risk_free_rate = 0.01, own_df_distribution = NULL) 
+{
+  if (base::is.null(x)) {
+    stock_data <- tidyquant::tq_get(ticker, from = date_from, 
+                                    to = date_to) %>% dplyr::select(date, adjusted) %>% 
+      stats::na.omit()
     x <- discount(p = stock_data$adjusted, r = risk_free_rate)
     date_x <- stock_data$date
   }
-  nrow_x <- length(x)
+  nrow_x <- base::length(x)
   window_size <- min_window
-  
-  t_val_model_nr <- case_when(
-    drift == F & trend == F ~ 1,
-    drift == T & trend == F ~ 2,
-    drift == T & trend == T ~ 3
-  )
-  distri_tibbles <- c(
-    "DF_distribution.RData",
-    "DF_distribution_drift.RData",
-    "DF_distribution_drift_trend.RData",
-    "DF_distribution_trend.RData"
-  )
-  load(distri_tibbles[t_val_model_nr])
-  distribution_tibble <- if (t_val_model_nr == 1) {
-    df_distri
-  } else if (t_val_model_nr == 2) {
-    df_distri_drift
-  } else if (t_val_model_nr == 3) {
-    df_distri_drift_trend
+  t_val_model_nr <- dplyr::case_when(drift == F & trend == 
+                                       F ~ 1, drift == T & trend == F ~ 2, drift == T & trend == 
+                                       T ~ 3)
+  if (base::is.null(own_df_distribution)) {
+    distribution_tibble <- if (t_val_model_nr == 1) {
+      df_distri
+    }
+    else if (t_val_model_nr == 2) {
+      df_distri_drift
+    }
+    else if (t_val_model_nr == 3) {
+      df_distri_drift_trend
+    }
   }
-  
-  
-  result <- tibble()
+  else {
+    distribution_tibble <- own_df_distribution
+  }
+  result <- tibble::tibble()
   k_ind <- 1
   while (window_size < (nrow_x - step_length)) {
-    max_i <- floor((nrow_x - window_size) / step_length)
+    max_i <- base::floor((nrow_x - window_size)/step_length)
     for (i in 0:max_i) {
-      x_window <- x[(step_length * i):((step_length * i) + window_size)]
-      plot_data <- tibble(t = seq_along(x_window), x_window)
+      x_window <- x[(step_length * i):((step_length * 
+                                          i) + window_size)]
+      plot_data <- tibble::tibble(t = base::seq_along(x_window), 
+                                  x_window)
       if (drift == F & trend == F) {
-        model <- lm(diff(x_window) ~ 0 + na.omit(lag(x_window)), data = plot_data)
+        model <- stats::lm(base::diff(x_window) ~ 0 + 
+                             stats::na.omit(dplyr::lag(x_window)), data = plot_data)
       }
       if (drift == T & trend == F) {
-        model <- lm(diff(x_window) ~ na.omit(lag(x_window)), data = plot_data)
+        model <- stats::lm(base::diff(x_window) ~ stats::na.omit(dplyr::lag(x_window)), 
+                           data = plot_data)
       }
       if (drift == T & trend == T) {
-        model <- lm(diff(x_window) ~ t[-1] + na.omit(lag(x_window)), data = plot_data)
+        model <- stats::lm(base::diff(x_window) ~ t[-1] + 
+                             stats::na.omit(dplyr::lag(x_window)), data = plot_data)
       }
-      coef_model <- coefficients(summary(model))
-      result[k_ind, "estimate"] <- coef_model[t_val_model_nr, 1]
-      result[k_ind, "std_error"] <- coef_model[t_val_model_nr, 2]
-      result[k_ind, "t_value"] <- coef_model[t_val_model_nr, 3]
+      coef_model <- stats::coefficients(base::summary(model))
+      result[k_ind, "estimate"] <- coef_model[t_val_model_nr, 
+                                              1]
+      result[k_ind, "std_error"] <- coef_model[t_val_model_nr, 
+                                               2]
+      result[k_ind, "t_value"] <- coef_model[t_val_model_nr, 
+                                             3]
       result[k_ind, "start_day"] <- step_length * i
-      result[k_ind, "end_day"] <- ((step_length * i) + window_size)
+      result[k_ind, "end_day"] <- ((step_length * i) + 
+                                     window_size)
       result[k_ind, "interval_length"] <- window_size
       k_ind <- k_ind + 1
     }
     window_size <- window_size + window_increase
   }
-  result <- result %>%
-    rowwise() %>%
-    mutate(p_val = p_val_fun(
-      t_val = t_value,
-      interval_length = interval_length,
-      distribution_tibble = distribution_tibble
-    ))
-  result_list <- list(
-    stock = tibble(
-      date = date_x,
-      price = x
-    ),
-    result = result
-  )
-  return(result_list)
+  result <- result %>% dplyr::rowwise() %>% dplyr::mutate(p_val = p_val_fun(t_val = t_value, 
+                                                                            interval_length = interval_length, distribution_tibble = distribution_tibble))
+  result_list <- base::list(stock = tibble::tibble(date = date_x, 
+                                                   price = x), result = result)
+  base::return(result_list)
 }
--- plot_GSADF.R
+++ plot_GSADF.R
@@ -1,54 +1,71 @@
 plot_GSADF <- function(u, d = NULL, d_t = NULL, p_restrict = 0.95, start_date_tq_get = "2020-01-01",
                        image_name = NULL, valuta = "valuta", aktie = "aktie") {
-  
-  model_nr <- case_when(is.null(d_t) & is.null(d) ~ 1,
-                        is.null(d_t) ~ 2,
-                        !is.null(d_t) ~ 3)
-  plot_data_combined <- combined_count(u = u, d_t = d_t, d = d, p_restrict = p_restrict)
-  
-  plot_data_rect <- u$result %>%
-    filter(p_val == max(u$result$p_val)) %>%
-    mutate(
-      date_start = ymd(start_date_tq_get) + start_day,
-      date_end = ymd(start_date_tq_get) + end_day
-    )
-  
-  
-  
-  stock <- ggplot(data = u$stock, aes(x = date, y = price)) +
-    geom_line(size = 1) +
-    geom_rect(
-      data = plot_data_rect, aes(xmin = date_start, xmax = date_end),
-      ymin = -Inf, ymax = Inf, alpha = 0.1, inherit.aes = F
+  model_nr <- dplyr::case_when(is.null(d_t) & is.null(d) ~
+  1, is.null(d_t) ~ 2, !is.null(d_t) ~ 3)
+  plot_data_combined <- combined_count(
+    u = u, d_t = d_t, d = d,
+    p_restrict = p_restrict
+  )
+  plot_data_rect <- tibble::tibble(u$result) %>%
+    dplyr::filter(p_val ==
+      max(p_val)) %>%
+    dplyr::filter(interval_length == max(interval_length)) %>%
+    dplyr::mutate(date_start = lubridate::ymd(start_date_tq_get) +
+      start_day, date_end = lubridate::ymd(start_date_tq_get) +
+      end_day)
+  stock <- ggplot2::ggplot(data = u$stock, ggplot2::aes(
+    x = date,
+    y = price
+  )) +
+    ggplot2::geom_line(size = 1) +
+    ggplot2::geom_rect(
+      data = plot_data_rect,
+      ggplot2::aes(xmin = date_start, xmax = date_end), ymin = -Inf,
+      ymax = Inf, alpha = 0.7, inherit.aes = F
     ) +
-    scale_x_date(date_labels = "%Y %b", date_breaks = "2 month") +
-    xlab("") +
-    ylab(paste("Price in", valuta)) +
-    labs(title = aktie) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
-  
-  
-  
-  
-  bubble <- ggplot(data = plot_data_combined, aes(x = x, y = value, color = name)) +
-    geom_line(size = 1) +
-    scale_x_date(date_labels = "%Y %b", date_breaks = "2 month") +
-    theme(legend.position = "bottom",
-          axis.text.x = ggplot2::element_text(angle = 60, hjust = 1)) +
-    xlab("") +
-    ylab("Count")
-  
+    ggplot2::scale_x_date(
+      date_labels = "%Y %b",
+      date_breaks = "2 month"
+    ) +
+    ggplot2::xlab("") +
+    ggplot2::ylab(paste(
+      "Price in",
+      valuta
+    )) +
+    ggplot2::labs(title = aktie) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(
+      angle = 60,
+      hjust = 1
+    ))
+  bubble <- ggplot2::ggplot(data = plot_data_combined, ggplot2::aes(
+    x = x,
+    y = value, color = name
+  )) +
+    ggplot2::geom_line(size = 1) +
+    ggplot2::scale_x_date(date_labels = "%Y %b", date_breaks = "2 month") +
+    ggplot2::theme(legend.position = "bottom", axis.text.x = ggplot2::element_text(
+      angle = 60,
+      hjust = 1
+    )) +
+    ggplot2::xlab("") +
+    ggplot2::ylab("Count")
   if (model_nr == 1) {
-    bubble <- bubble + theme(legend.position = "none")
+    bubble <- bubble + ggplot2::theme(legend.position = "none")
   }
-  
   if (!is.null(image_name)) {
     file_name <- paste(image_name, ".pdf", sep = "")
-    pdf(file = file_name, height = 4,width = 8)
-    grid.arrange(stock, bubble, nrow = 2, heights = c(2,1))
-    dev.off()
+    grDevices::pdf(file = file_name, height = 4, width = 8)
+    gridExtra::grid.arrange(stock, bubble, nrow = 2, heights = c(
+      3,
+      2
+    ))
+    grDevices::dev.off()
     cat("image found in ", file_name)
-  } else {
-    grid.arrange(stock, bubble, nrow = 2, heights = c(2,1))
-    }
+  }
+  else {
+    gridExtra::grid.arrange(stock, bubble, nrow = 2, heights = c(
+      3,
+      2
+    ))
+  }
 }
